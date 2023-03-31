125baf214b69f5e13b24aebb3d39c4d01e28cbd9
Dipak Shrestha <shresthadipak2049@gmail.com>
2023-03-31 17:12:04 +0545

    Initial Commit

---
+++ src/main/java/com/apps/blog/BlogAppsApisApplication.java
@@ -0,0 +1,13 @@
+package com.apps.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppsApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppsApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apps/blog/BlogAppsApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppsApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
